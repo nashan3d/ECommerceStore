43a4bfb0e427d2227604c2b3bd13c77d6ee01bbc
Mohamed Nashan <nashan@bistecglobal.com>
2023-05-27 17:44:31 +0530

    Implement Product Controller to expose product data

--- ECommerceStore.API/Controllers/ProductsController.cs
+++ ECommerceStore.API/Controllers/ProductsController.cs
@@ -1,16 +1,38 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ECommerceStore.Core.Products.Commands;
+using ECommerceStore.Core.Products.Commands.EditProduct;
+using ECommerceStore.Core.Products.Queries.ViewProduct;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceStore.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseController
     {
 
-        public async Task<IActionResult> CreateProduct()
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDto input)
         {
+            var result = await Mediator.Send(new CreateProductCommand(input));
 
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditProduct(EditProductDto input)
+        {
+            var result = await Mediator.Send(new EditProductCommand(input));
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ViewProduct(Guid id)
+        {
+            var result = await Mediator.Send(new ViewProductQuery(id));
+
+            return Ok(result);
         }
     }
 }

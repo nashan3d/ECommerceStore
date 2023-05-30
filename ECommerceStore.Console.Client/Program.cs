d06c7ba526254eb5e2b96990465022eae7e0a06c
Mohamed Nashan <nashan@bistecglobal.com>
2023-05-30 20:15:49 +0530

    Add Console Client Test API

---
+++ ECommerceStore.Console.Client/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+Console.WriteLine("API Testing Starts!");
+
+
+var client = new HttpClient();
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5443");
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    return;
+}
+
+var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+{
+    Address = disco.TokenEndpoint,
+
+    ClientId = "client",
+    ClientSecret = "secret",
+    Scope = "ECommerceStore.API"
+});
+
+if (tokenResponse.IsError)
+{
+    Console.WriteLine(tokenResponse.Error);
+    return;
+}
+
+Console.WriteLine(tokenResponse.Json);
+
+var apiClient = new HttpClient();
+apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+var response = await apiClient.GetAsync("https://localhost:7251/api/Products?id=3FA85F64-5717-4562-B3FC-2C963F66AFA6");
+if (!response.IsSuccessStatusCode)
+{
+    Console.WriteLine(response.StatusCode);
+}
+else
+{
+    var content = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(content);
+}
